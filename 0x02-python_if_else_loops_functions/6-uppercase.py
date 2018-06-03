db309ac27c9cb1028011c391ce65dce1cb39f84e
juanjosr10 <juanjosr10@gmail.com>
2018-06-03 22:56:49 +0000

    task 6

---
+++ 0x02-python_if_else_loops_functions/6-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    for i in range(len(str)):
+        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
+            var = ord(str[i]) - 32
+        else:
+            var = ord(str[i])
+        print("{}".format(chr(var)), end='')
+    print("")
+    
